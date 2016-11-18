0cce7a9acd16f600a1dacc6313ac84fa2026d33f
shark <sharkshore@163.com>
2016-11-18 14:25:53 +0800

    第二次提交

--- components/Two.jsx
+++ components/Two.jsx
@@ -4,7 +4,7 @@ export default class Two extends React.Component {
     render() {
         return (
             <div>
-                <h2>第二个网页</h2>
+                <h2>第二个网页,第二次提交</h2>
                 <TypographyExample/>
             </div>
         );
